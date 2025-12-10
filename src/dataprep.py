ff903d88e68627b37c38af1ff774557930428c4e
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-12-10 15:48:16 +0100

    setup file structure

---
+++ src/dataprep.py
@@ -0,0 +1,1 @@
+# this file will contain data loading and masking logic
---
+++ src/model.py
@@ -0,0 +1,10 @@
+from torch.nn import Module
+
+class Encoder(Module):
+    pass  
+
+class Predicator(Module):
+    pass 
+
+class JEPA(Module):
+    pass
