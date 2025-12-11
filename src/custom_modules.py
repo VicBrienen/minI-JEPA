71dd1e5721e4ebdd34cc3fc8b602cab0dbd815fa
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-12-11 16:01:43 +0100

    add positional embedding to encoder

--- src/custom_modules.py
+++ src/custom_modules.py
@@ -2,14 +2,14 @@ import torch
 from torch import nn
 
 class PatchEmbedding(nn.Module):
-    def __init__(self, in_channels, emb_dim, patch_size):
+    def __init__(self, in_channels, embed_dim, patch_size):
         super().__init__()
         self.in_channels = in_channels
-        self.emb_dim = emb_dim
+        self.embed_dim = embed_dim
         self.patch_size = patch_size
 
         # embed using convolution with stride = patch_size
-        self.embed = nn.Conv2d(in_channels, emb_dim, patch_size, patch_size)
+        self.embed = nn.Conv2d(in_channels, embed_dim, patch_size, patch_size)
     
     def forward(self, x):           # (B, C, H, W)
         x = self.embed(x)           # (B, E, H, W)
--- src/model.py
+++ src/model.py
@@ -1,13 +1,18 @@
+import torch
 from torch import nn
 from custom_modules import PatchEmbedding
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels, emb_dim, patch_size):
+    def __init__(self, in_channels, embed_dim, patch_size, img_size):
         super().__init__()
-        self.patch_embedding = PatchEmbedding(in_channels, emb_dim, patch_size)
+        self.patch_embed = PatchEmbedding(in_channels, embed_dim, patch_size)
+        self.num_patches = (img_size // patch_size) ** 2
 
-    def forward(self, x):               # (B, C, H, W)
-        x = self.patch_embedding(x)     # (B, N, E)
+        self.pos_embed = nn.Parameter(torch.randn(1, self.num_patches, embed_dim)) # (B, N, E) -> positional embedding for each patch and same size as embed_dim
+
+    def forward(self, x):           # (B, C, H, W)
+        x = self.patch_embed(x)     # (B, N, E)
+        x = x + self.pos_embed      # (B, N, E)
 
 class Predicator(nn.Module):
     pass 
