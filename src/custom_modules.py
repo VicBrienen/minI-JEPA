e0ff2e214546c256d91282a4cfdf7cb9d1cb57bd
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-12-11 15:37:18 +0100

    add patchembedding to encoder

--- src/custom_modules.py
+++ src/custom_modules.py
@@ -7,10 +7,11 @@ class PatchEmbedding(nn.Module):
         self.in_channels = in_channels
         self.emb_dim = emb_dim
         self.patch_size = patch_size
+
+        # embed using convolution with stride = patch_size
         self.embed = nn.Conv2d(in_channels, emb_dim, patch_size, patch_size)
     
-    def forward(self, x):
-        x = self.embed(x)
-        x = x.flatten(2)
-        x = x.transpose(1, 2)
-        return 
+    def forward(self, x):           # (B, C, H, W)
+        x = self.embed(x)           # (B, E, H, W)
+        x = x.flatten(2)            # (B, E, N)
+        return x.transpose(1, 2)    # (B, N, E)
--- src/model.py
+++ src/model.py
@@ -1,10 +1,16 @@
-from torch.nn import Module
+from torch import nn
+from custom_modules import PatchEmbedding
 
-class Encoder(Module):
-    pass  
+class Encoder(nn.Module):
+    def __init__(self, in_channels, emb_dim, patch_size):
+        super().__init__()
+        self.patch_embedding = PatchEmbedding(in_channels, emb_dim, patch_size)
 
-class Predicator(Module):
+    def forward(self, x):               # (B, C, H, W)
+        x = self.patch_embedding(x)     # (B, N, E)
+
+class Predicator(nn.Module):
     pass 
 
-class JEPA(Module):
+class JEPA(nn.Module):
     pass
