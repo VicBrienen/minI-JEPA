3161ce0143af45d417a6023c465f0656c4616046
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-12-14 13:19:53 +0100

    implement sin-cos positional embeddings in the Encoder

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from custom_modules import PatchEmbedding, TransformerBlock
+from helper import get_2d_pos_embed
 
 class Encoder(nn.Module):
     def __init__(self, in_channels, img_size, patch_size, embed_dim, depth, heads, mlp_ratio):
@@ -8,7 +9,11 @@ class Encoder(nn.Module):
 
         # patch embedding
         self.patch_embed = PatchEmbedding(in_channels, embed_dim, patch_size)       # (B, T, embed_dim)
-        self.num_patches = (img_size // patch_size) ** 2
+
+        # 2d sin-cos positional embedding
+        grid_size = img_size // patch_size
+        pos_embed = get_2d_pos_embed(embed_dim, grid_size)
+        self.register_buffer("pos_embed", pos_embed.unsqueeze(0)) # (1, T, embed_dim)
 
         # transformer blocks
         self.blocks = nn.ModuleList([
@@ -18,4 +23,12 @@ class Encoder(nn.Module):
         self.norm = nn.LayerNorm(embed_dim)
 
     def forward(self, x):
-        pass
+        # patch embedding + positional embedding
+        x = self.patch_embed(x) + self.pos_embed
+
+        # pass trough transformer blocks
+        for block in self.blocks:
+            x = block(x)
+
+        # normalize and return
+        return self.norm(x)
