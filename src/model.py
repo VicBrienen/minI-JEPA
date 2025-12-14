be70e06cca4af0bcd8e270fa0124723017eff233
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-12-14 12:44:08 +0100

    fix Encoder by removing incorrect positional embeddings

--- src/model.py
+++ src/model.py
@@ -6,23 +6,16 @@ class Encoder(nn.Module):
     def __init__(self, in_channels, img_size, patch_size, embed_dim, depth, heads, mlp_ratio):
         super().__init__()
 
-        # patch embedding + positional encoding
+        # patch embedding
         self.patch_embed = PatchEmbedding(in_channels, embed_dim, patch_size)       # (B, T, embed_dim)
         self.num_patches = (img_size // patch_size) ** 2
-        self.pos_embed = nn.Parameter(torch.randn(1, self.num_patches, embed_dim))  # (B, T, embed_dim)
 
         # transformer blocks
         self.blocks = nn.ModuleList([
             TransformerBlock(embed_dim, heads, mlp_ratio) for _ in range(depth)
         ])
 
-    def forward(self, x):                           # (B, C, H, W)
-        x = self.patch_embed(x) + self.pos_embed    # (B, T, embed_dim)
-        x = self.blocks(x)
-        return self.norm(x)                         # (B, T, embed_dim)
+        self.norm = nn.LayerNorm(embed_dim)
 
-class Predicator(nn.Module):
-    pass 
-
-class JEPA(nn.Module):
-    pass
+    def forward(self, x):
+        pass
