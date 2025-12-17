876bb6fd19f8574a4ac1e31eb50a19c5cedafdb7
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2025-12-17 17:57:40 +0100

    implement sample_mask which returns indices for a single mask (sticking to single mask because we are building for educational purposes)

--- src/dataprep.py
+++ src/dataprep.py
@@ -1,1 +1,18 @@
-# this file will contain data loading and masking logic
+import torch
+
+def sample_mask(grid_size, block_size):
+    # generate top left indice
+    h = torch.randint(0, grid_size - block_size + 1, (1,))
+    w = torch.randint(0, grid_size - block_size + 1, (1,))
+
+    # create 2d boolean mask over the patch grid (true = masked, false = visible)
+    mask = torch.zeros(grid_size, grid_size, dtype=torch.bool)
+    mask[h:h+block_size, w:w+block_size] = True
+
+    mask = mask.flatten()
+
+    # indices of visible and masked tokens
+    visible_idx = torch.nonzero(~mask).squeeze(1)
+    masked_idx = torch.nonzero(mask).squeeze(1)
+
+    return visible_idx, masked_idx
